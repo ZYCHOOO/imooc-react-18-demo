8a95fa310cf35adb7604627f56197594f039f1f7
ZYCHOOO <931187106@qq.com>
2024-09-25 00:03:11 +0800

    perf：优化模态框展示

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,11 +1,12 @@
 /*
  * @Date: 2024-09-24 15:18:59
  * @LastEditors: 曾逸超
- * @LastEditTime: 2024-09-24 22:20:52
+ * @LastEditTime: 2024-09-25 00:02:02
  * @FilePath: /react-learn/huanlegou/src/components/Modal/index.tsx
  */
-import { useState, forwardRef, useImperativeHandle } from 'react';
 import './style.scss';
+import { createPortal } from 'react-dom';
+import { useState, forwardRef, useImperativeHandle, useRef, useEffect } from 'react';
 
 export type ModalRefType = {
   showModal: (message: string) => void
@@ -14,6 +15,8 @@ export type ModalRefType = {
 const Modal = forwardRef((props, ref) => {
   const [modalVisible, setModalVisible] = useState(false);
   const [message, setMessage] = useState('');
+  const divElementRef = useRef(document.createElement('div'));
+  const divElement = divElementRef.current;
 
   useImperativeHandle(ref, () => {
     return {
@@ -27,11 +30,27 @@ const Modal = forwardRef((props, ref) => {
     }
   }, [])
 
-  return modalVisible ? (
+  useEffect(() => {
+    if (modalVisible) {
+      document.body.appendChild(divElement);
+    } else {
+      if (divElement.parentNode) {
+        document.body.removeChild(divElement);
+      }
+    }
+
+    return () => {
+      if (divElement.parentNode) {
+        document.body.removeChild(divElement);
+      }
+    }
+  }, [modalVisible, divElement]);
+
+  return createPortal(
     <div className="modal">
       <div className="modal-text">{ message }</div>
     </div>
-  ) : null;
+  , divElement);
 })
 
 export default Modal;
